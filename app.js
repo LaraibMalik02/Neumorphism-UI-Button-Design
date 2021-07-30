d46b399a57bd63513ac6bd99ddda47f4c7337938
Laraib Malik <77496843+LaraibMalik02@users.noreply.github.com>
2021-07-30 17:00:54 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+const toggleBtns = document.querySelectorAll('.toggle-box');
+toggleBtns.forEach((btn) =>{
+    btn.addEventListener('click', () =>{
+        btn.classList.toggle('toggle-pressed');
+    });
+});
